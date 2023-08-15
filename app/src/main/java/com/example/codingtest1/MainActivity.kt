2e24d464c2a01e24fe1be9f516022f5fbf9e0d44
Theekshana <tvimukthi.m@gmail.com>
2023-08-15 21:06:50 +0900

    add data binding

--- app/src/main/java/com/example/codingtest1/MainActivity.kt
+++ app/src/main/java/com/example/codingtest1/MainActivity.kt
@@ -3,30 +3,30 @@ package com.example.codingtest1
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import com.example.codingtest1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var recyclerView: RecyclerView
+
     private lateinit var viewModel: ContactsViewModel
     private lateinit var adapter: ContactsAdapter
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         // Initialize the ViewModel using ViewModelProvider
         viewModel = ViewModelProvider(this)[ContactsViewModel::class.java]
 
-        // Initialize the RecyclerView
-        recyclerView = findViewById(R.id.contactsRecyclerView)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.contactsRecyclerView.setHasFixedSize(true)
+        binding.contactsRecyclerView.layoutManager = LinearLayoutManager(this)
 
         // Initialize the RecyclerView Adapter with the initial contact list
         adapter = ContactsAdapter(viewModel.contacts.value ?: emptyList())
-        recyclerView.adapter = adapter
+        binding.contactsRecyclerView.adapter = adapter
 
         // Observe the contacts LiveData for changes
         viewModel.contacts.observe(this) { contactsList ->
