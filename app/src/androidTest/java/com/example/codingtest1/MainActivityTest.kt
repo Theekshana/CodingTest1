a0f74e323c4dbb3c4775f51ac33d9639da75fcbd
Theekshana <tvimukthi.m@gmail.com>
2023-09-08 17:54:33 +0530

    unit test for edit contact

---
+++ app/src/androidTest/java/com/example/codingtest1/MainActivityTest.kt
@@ -0,0 +1,40 @@
+package com.example.codingtest1
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import okhttp3.internal.wait
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class MainActivityTest {
+
+    @Rule
+    @JvmField
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun testAddContactButton() {
+
+        onView(withId(R.id.btnAddContact)).perform(click())
+
+        onView(withId(R.id.etName)).perform(typeText("John"), closeSoftKeyboard())
+        onView(withId(R.id.etNumber)).perform(typeText("123456789"), closeSoftKeyboard())
+        onView(withId(R.id.etDescription)).perform(typeText("Friend"), closeSoftKeyboard())
+
+        onView(withId(android.R.id.button1)).perform(click())
+
+    }
+
+}
+
--- app/src/test/java/com/example/codingtest1/ContactsViewModelTest.kt
+++ app/src/test/java/com/example/codingtest1/ContactsViewModelTest.kt
@@ -50,10 +50,10 @@ internal class ContactsViewModelTest {
     fun testLoadContacts() {
 
         val testContacts = arrayListOf(
-            ContactsData("name", "123456789", "description"),
-            ContactsData("name", "123456789", "description")
+            ContactsData("John", "12345", "Friend"),
+            ContactsData("Alice", "67890", "Family")
         )
-        
+
         viewModel.contacts.observeForever(observer)
 
         viewModel.loadContacts(testContacts)
@@ -68,5 +68,29 @@ internal class ContactsViewModelTest {
 
     }
 
+    @Test
+    fun testEditContact() {
+
+        val initialContacts = arrayListOf(
+            ContactsData("John", "12345", "Friend"),
+            ContactsData("Alice", "67890", "Family"),
+            ContactsData("Bob", "54321", "Colleague")
+        )
+
+        viewModel.loadContacts(initialContacts)
+
+        viewModel.contacts.observeForever(observer)
+
+        val newContact = ContactsData("John Doe", "12345", "Best Friend")
+
+        viewModel.editContact(newContact)
+
+        Mockito.verify(observer, Mockito.atLeastOnce()).onChanged(Mockito.any())
+
+        val updatedContacts = viewModel.contacts.value
+
+        assert(updatedContacts != null)
+        assert(updatedContacts!!.contains(newContact))
+    }
 
 }
