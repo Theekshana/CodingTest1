cc5c348c0d16cccd1a3cb166f63ec1310d04ecd5
Theekshana <tvimukthi.m@gmail.com>
2023-10-07 15:37:21 +0900

    create room db class

--- app/src/main/java/com/example/codingtest1/localDB/ContactsListDB.kt
+++ app/src/main/java/com/example/codingtest1/localDB/ContactsListDB.kt
@@ -1,9 +1,32 @@
 package com.example.codingtest1.localDB
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.codingtest1.model.ContactsData
 
+@Database(entities = [ContactsData::class], version = 1, exportSchema = false)
+abstract class ContactsListDB: RoomDatabase(){
+    abstract fun contactsDao(): ContactsDao
 
-class ContactsListDB{
+    companion object {
+        @Volatile
+        private var INSTANCE: ContactsListDB? = null
+        fun getInstance(context: Context): ContactsListDB {
+
+            return INSTANCE
+                ?: synchronized(this) {
+                    INSTANCE ?: Room.databaseBuilder(
+                        context.applicationContext,
+                        ContactsListDB::class.java, "contacts"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                        .also { INSTANCE = it }
+                }
+        }
+    }
+
 
 }
